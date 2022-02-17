c393c3c0b14626f43440cbc719d0e6dc164e9bb9
Halley Rios <HalleyRios@gmail.com>
2022-02-17 17:43:29 -0300

    feat: Adds `Queue` data structure with tests

---
+++ src/DataStructures/Queue.test.ts
@@ -0,0 +1,41 @@
+import Queue from "./Queue";
+
+describe('Queue', () => {
+    const FIRST_ITEM_INDEX = 0;
+    const items: number[] = [1, 2, 3, 4, 5];
+
+    it('should be an instance of Queue', () => {
+        const testQueue = new Queue();
+
+        expect(testQueue).toBeInstanceOf(Queue);
+    });
+
+    it('should enqueue elements in correct order', () => {
+        const testQueue = new Queue();
+        items.map(element => testQueue.enqueue(element));
+
+        expect(testQueue.values[FIRST_ITEM_INDEX]).toEqual(items[FIRST_ITEM_INDEX]);
+    });
+
+    it('should dequeue elements in correct order', () => {
+        const testQueue = new Queue();
+        items.map(element => testQueue.enqueue(element));
+        const dequeuedElement = testQueue.dequeue();
+        expect(dequeuedElement).toEqual(items[FIRST_ITEM_INDEX]);
+        expect(testQueue.size).toEqual(items.length - 1);
+    });
+
+    it('should return the first element in a queue', () => {
+        const testQueue = new Queue();
+        items.map(element => testQueue.enqueue(element));
+        expect(testQueue.front()).toEqual(items[FIRST_ITEM_INDEX]);
+    });
+
+    it('should correctly report if queue is empty', () => {
+        const emptyQueue = new Queue();
+        const filledQueue = new Queue();
+        items.map(element => filledQueue.enqueue(element));
+        expect(emptyQueue.isEmpty).toBeTruthy();
+        expect(filledQueue.isEmpty).toBeFalsy();
+    });
+});
---
+++ src/DataStructures/Queue.ts
@@ -0,0 +1,35 @@
+class Queue {
+    static FIRST_ELEMENT_INDEX = 0;
+
+    collection: Array<any>;
+
+    constructor() {
+        this.collection = new Array<any>();
+    }
+
+    enqueue(element: any): void {
+        this.collection.push(element);
+    }
+
+    dequeue(): any {
+        return this.collection.shift();
+    }
+
+    front(): any {
+        return this.collection[Queue.FIRST_ELEMENT_INDEX];
+    }
+
+    get size(): number {
+        return this.collection.length;
+    }
+
+    get isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+    get values(): Array<any> {
+        return this.collection;
+    }
+}
+
+export default Queue;
